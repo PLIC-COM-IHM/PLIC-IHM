b1abc8e49289f0e8c855ca23517257dbe7ac6fb0
Vincent Oliveira <vincent@vincent-virtual-ubuntu.(none)>
2012-04-16 19:47:19 +0200

    Projects parser

--- app/js/parseprojects.js
+++ app/js/parseprojects.js
@@ -1,8 +1,6 @@
 // Parse projects folder and insert results in database
 function parseProjects() {
-    if (debug) {
-        $('#debug').append("parseProject()<br/>");
-    }
+    console.debug("parseProject()");
     
     $.ajax({ 
         type: "GET", 
@@ -11,11 +9,9 @@ function parseProjects() {
     });
 }
 
-function parseProjectsHtml(data)
-{
-    if (debug) {
-        $('#debug').append('parseProjectHtml(' + data + ')<br/>');
-    }
+// Parse 'projects' folder
+function parseProjectsHtml(data) {
+    //console.debug('parseProjectHtml(' + data + ')');
     
     var results = new Array(), intd = false;
     HTMLParser(data, {
@@ -42,13 +38,76 @@ function parseProjectsHtml(data)
     // remove back link
     results.shift();
     
-    if (debug) {
-        $('#debug').append(results.length + ' result(s) found');
-    }
+    console.debug(results.length + ' result(s) found');
     
-    $('#test').append("<ul>"); 
     for (var i = 0; i < results.length; i++) {
-        $('#test').append("<li>" + results[i] + "</li>"); 
+        parseProjectFolder('projects/' + results[i]);
     }
-    $('#test').append("</ul>"); 
+}
+
+// parse a project folder and put extracted data in database 
+function parseProjectFolder(folder) {
+    var json = folder + 'project.json'
+    //console.debug('parseProjectFolder(' + json + ')');
+    
+    $.getJSON(json, function(data) {
+        // get images
+        $.ajax({ 
+            type: "GET", 
+            url: folder + "media/images/",
+            success: function (media) {
+                data.images = parseMediaFolder(media);
+            },
+            async: false
+        });
+        
+        // get videos
+        $.ajax({ 
+            type: "GET", 
+            url: folder + "media/videos/",
+            success: function (media) {
+                data.videos = parseMediaFolder(media);
+            },
+            async: false
+        });
+        
+        console.debug('project: ' + data.name);
+        console.debug('project-images: ' + data.images);
+        console.debug('project-videos: ' + data.videos);
+    });
+}
+
+
+// Parse media folder. Return an array
+function parseMediaFolder(media) {
+    //console.debug('parseMediaFolder(' + media + ')');
+    
+    var results = new Array(), intd = false;
+    HTMLParser(media, {
+        start: function(tag, attrs, unary) {
+            if (tag == "td") {
+                intd = true;
+            } else if (intd && tag == "a") {
+                // find href attributes in <a>
+                for (var i = 0; i < attrs.length; i++) {
+                    if (attrs[i].name == "href") {
+                        results.push(attrs[i].value);
+                    }
+                }
+            }
+        },
+        
+        end: function(tag) {
+            if (tag == "td") {
+                intd = false;
+            }
+        }
+    });
+    
+    // remove back link
+    results.shift();
+    
+    console.debug(results.length + ' media found');
+
+    return results; 
 }
