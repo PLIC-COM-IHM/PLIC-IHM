e83b91e47e0265e71273ae3e1cc17e94c411dd5f
Vincent Oliveira <vincent@Vincents-MacBook-Pro.local>
2012-05-02 12:53:01 +0200

    Empty index.html

--- app/js/init.js
+++ app/js/init.js
@@ -1,4 +1,10 @@
+function showLoading(where) {
+    $(where).empty()
+    .append('<img class"loading" src="images/loading.gif" alt="Loading..."/>');
+}
+
 $(document).ready(function() {
+    showLoading("#content");
     databaseInit();
     parseProjects();
 });
