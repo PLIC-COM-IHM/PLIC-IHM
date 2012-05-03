87da378a6523e75311c64d543eb73885daa7da80
Vincent Oliveira <vincent@Vincents-MacBook-Pro.local>
2012-05-03 18:27:45 +0200

    Instanciate tile in homepage

---
+++ app/js/homepage.js
@@ -0,0 +1,30 @@
+function homepage() {
+    console.debug('Trying to instanciate homepage...');
+    dbGetAllCategories(hpInstantiateCategorie);
+}
+
+function hpInstantiateCategorie(categories) {
+    // db filled ?
+    if (categories.length == 0) { window.setTimeout(homepage, 500); return; }
+
+    console.debug('Instanciate home page categories');
+    $("#content").empty()
+    .append('<div id="categorytilelist" class="tilelist"><h3>Categories</h3></div>');
+    
+    for (i=0; i<categories.length; i++) {
+        $("#categorytilelist").append(categories[i].categoryName);
+    }
+    
+    dbGetAllTechnologies(hpInstantiateTechnologies);
+}
+
+function hpInstantiateTechnologies(technologies) {
+    console.debug('Instanciate home page categories');
+    $("#content").append('<div id="technologiestilelist" class="tilelist"><h3>Technologies</h3></div>');
+    
+    for (i=0; i<technologies.length; i++) {
+        $("#technologiestilelist").append(technologies[i].technoName);
+    }
+    
+    //dbGetAllTechnologies(hpInstantiateCategorie);
+}
