8860fbd05512f8e5333ff74d98718f7e324dd3f8
Benjamin Levoir <ben.xpo@gmail.com>
2012-05-07 00:55:28 +0200

    Add first version of keyboard

---
+++ clavier/clavier.js
@@ -0,0 +1,30 @@
+$(document).ready(
+	function()
+	{
+		
+	}
+);
+
+function addkey (text)
+{
+	document.getElementById("champ").value += text.innerHTML;
+}
+
+function spacekey ()
+{
+	document.getElementById("champ").value += " ";
+}
+
+function remkey ()
+{
+	
+	document.getElementById("champ").value =
+		document.getElementById("champ").value.slice(
+			0, document.getElementById("champ").value.length - 1);
+}
+
+function enterkey ()
+{
+	result = document.getElementById("champ").value;
+	
+}
