0641ff3468981d81c9067392eecc456c46d98323
vincentoliveira <vincent.oliveira@epita.fr>
2012-11-03 11:23:47 +0100

    Images gallery anim

--- app/js/images.js
+++ app/js/images.js
@@ -18,16 +18,30 @@ function createPage(p)
 	
 	project = p;
 	
+	/*
+	for (i=0; i<project.images.length; ++i) {
+		image = project.folder + "media/images/" + project.images[i];
+		item = '<li><img src="' + image + '" alt="" style="height:600px; width:800px;" /></li>';
+		$('ul').append(item);
+		console.debug(item);
+	}
+	*/
         $('#media').attr('src', project.folder + "media/images/" + project.images[index]);
 }
 
 function ileft ()
 {
         index = (index - 1 + project.images.length) % project.images.length;
-        $('#media').attr('src', project.folder + "media/images/" + project.images[index]);
+	$('#media').fadeOut('fast', function () {
+		$('#media').attr('src', project.folder + "media/images/" + project.images[index]);
+		$('#media').fadeIn();
+	});
 }
 function iright()
 {
         index = (index + 1) % project.images.length;
-        $('#media').attr('src', project.folder + "media/images/" + project.images[index]);
+	$('#media').fadeOut('fast', function () {
+		$('#media').attr('src', project.folder + "media/images/" + project.images[index]);
+		$('#media').fadeIn();
+	});
 }
